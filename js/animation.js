ebec6d36b44283afe629a8e05f549a6979f782e6
KaterynaKocheharova <kot41901@gmail.com>
2024-03-05 22:02:25 +0200

    mousemove events experiments

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+// const movingElement = document.querySelector(".animation-span");
+
+// document.addEventListener("mousemove", onMouseMoveAnimation);
+
+// function onMouseMoveAnimation(event) {
+//     movingElement.style.top = `${event.pageY}px`;
+//     movingElement.style.left = `${event.pageX}px`;
+// }
+
