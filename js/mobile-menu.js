bbb96c4d00772fc78b3652eb61285ccc9deb3075
KaterynaKocheharova <kot41901@gmail.com>
2024-03-11 20:26:46 +0200

    some functions made reusable

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,7 +6,7 @@ const mobileMenu = document.querySelector('.mobile-menu-backdrop');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-button');
 const mobileMenuList = document.querySelector('.mobile-menu-list');
 
-// ==================================== OPENING MODAL
+// // ==================================== OPENING MODAL
 
 // function openModal() {
 //   mobileMenu.classList.add('is-open');
@@ -17,37 +17,78 @@ const mobileMenuList = document.querySelector('.mobile-menu-list');
 
 // mobileMenuBurger.addEventListener('click', openModal);
 
-function openModal(modalElement) {
-  modalElement.classList.add('is-open');
-  // document.addEventListener('keydown', (event) => escCloseModal(event, mobileMenu));
-  // mobileMenuList.addEventListener('click', closeModal);
-  // mobileMenuCloseBtn.addEventListener('click', closeModal);
+// // ==================================== CLOSING MODAL
+
+// function closeModal() {
+//   mobileMenu.classList.remove('is-open');
+//   removeModalEventListenersOnClose();
+// }
+
+// // ================================ REMOVING EVENT LISTENERS
+
+// function removeModalEventListenersOnClose() {
+//   document.removeEventListener('keydown', (event) => escCloseModal(event, mobileMenu));
+//   mobileMenuList.removeEventListener('click', closeModal);
+//   mobileMenuCloseBtn.removeEventListener('click', closeModal);
+// }
+
+// // =============================== CLOSING MODAL ON ESCAPE
+
+// function escCloseModal(event, modalElement) {
+//   if (event.key === 'Escape' && modalElement.classList.contains('is-open')) {
+//     closeModal(modalElement);
+//     console.log("Modal is closed");
+//   }
+// }
+
+// !!! new version
+
+// ==================================== OPENING MODAL
+
+mobileMenuBurger.addEventListener('click', () => openModal(mobileMenu));
+mobileMenuBurger.addEventListener('click', () => onModalOpen(mobileMenuList, mobileMenuCloseBtn, mobileMenu));
+mobileMenuBurger.addEventListener('click', () => onModalClose(mobileMenuList, mobileMenuCloseBtn, mobileMenu));
+
+function openModal(modalToOpen) {
+  modalToOpen.classList.add('is-open');
 }
 
-mobileMenuBurger.addEventListener('click', (event) => openModal(mobileMenu));
-
-
-
-// ==================================== CLOSING MODAL
-
-function closeModal() {
-  mobileMenu.classList.remove('is-open');
-  removeModalEventListenersOnClose();
+function closeModal(modalToClose) {
+  modalToClose.classList.remove('is-open');
 }
 
-// ================================ REMOVING EVENT LISTENERS
-
-function removeModalEventListenersOnClose() {
-  document.removeEventListener('keydown', (event) => escCloseModal(event, mobileMenu));
-  mobileMenuList.removeEventListener('click', closeModal);
-  mobileMenuCloseBtn.removeEventListener('click', closeModal);
+function onModalOpen(list, closeBtn, modal) {
+  list.addEventListener('click', () => closeModal(modal));
+  closeBtn.addEventListener('click', () => closeModal(modal));
+  // document.addEventListener('keydown', () => {
+  //   document.addEventListener('keydown', () => {
+  //     if (event.key === 'Escape' && modalToClose.classList.contains('is-open')) 
+  //     {
+  //       closeModal(modalElement);
+  //       console.log("Modal is closed");
+  //     }
+  
+  //   })
+  
+  // })
 }
 
-// =============================== CLOSING MODAL ON ESCAPE
-
-function escCloseModal(event, modalElement) {
-  if (event.key === 'Escape' && modalElement.classList.contains('is-open')) {
-    closeModal(modalElement);
-    console.log("Modal is closed");
-  }
+function onModalClose(list, closeBtn, modal) {
+  list.removeEventListener('click', () => closeModal(modal));
+  closeBtn.removeEventListener('click', () => closeModal(modal));
+  // document.removeEventListener('keydown')
 }
+
+
+
+
+
+
+// function removeModalEventListenersOnClose() {
+//   document.removeEventListener('keydown', (event) => escCloseModal(event, mobileMenu));
+//   mobileMenuList.removeEventListener('click', closeModal);
+//   mobileMenuCloseBtn.removeEventListener('click', closeModal);
+// }
+
+// // =============================== CLOSING MODAL ON ESCAPE
+
