b16619fd7acfa2d89a13bf513c928fffe1bc10cf
KaterynaKocheharova <kot41901@gmail.com>
2024-03-12 18:32:46 +0200

    123

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,15 +8,28 @@ const mobileMenuList = document.querySelector('.mobile-menu-list');
 
 // ==================================== OPENING AND CLOSING MODAL
 
-mobileMenuBurger.addEventListener('click', event => toggleModal(mobileMenu));
-mobileMenuCloseBtn.addEventListener('click', event => toggleModal(mobileMenu));
-mobileMenuList.addEventListener('click', event => toggleModal(mobileMenu));
-document.addEventListener('keydown', event => {
-  if (event.code === 'Escape' && mobileMenu.classList.contains('is-open')) {
-    toggleModal(mobileMenu);
-  }
-});
+mobileMenuBurger.addEventListener('click', openModal);
+
+function openModal() {
+  toggleModal(mobileMenu);
+  mobileMenuCloseBtn.addEventListener('click', closeModal);
+  mobileMenuList.addEventListener('click', closeModal);
+  document.addEventListener("keydown", (event) => closeModalOnEsc(event, mobileMenu));
+}
+
+function closeModal() {
+  toggleModal(mobileMenu);
+  mobileMenuCloseBtn.removeEventListener('click', closeModal);
+  mobileMenuList.removeEventListener('click', closeModal);
+}
 
 function toggleModal(modal) {
   modal.classList.toggle('is-open');
 }
+
+function closeModalOnEsc(event, modal) {
+if(event.code === "Escape" && modal.classList.contains("is-open")) {
+  modal.classList.toggle("is-open")
+}
+}
+
