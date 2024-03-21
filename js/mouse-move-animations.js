fe7c81d8eab4b69ce55f7e3d62fcea0e6a457282
KaterynaKocheharova <kot41901@gmail.com>
2024-03-21 21:22:40 +0200

    added first custom cursor exmaple

---
+++ js/mouse-move-animations.js
@@ -0,0 +1,8 @@
+// const cursor = document.querySelector('.cursor');
+// const cursor2 = document.querySelector('.cursor2');
+
+// document.addEventListener('mousemove', customCursor);
+
+// function customCursor(event) {
+// cursor.style.cssText = cursor2.style.cssText = `left:${event.clientX}px; top:${event.clientY}px`;
+// }
