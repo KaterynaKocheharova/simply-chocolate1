a9fd257751abe6a97a9ffa1fac71bb411f3169d6
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-06 14:38:38 +0300

    Add slider to ingredients section

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+
+function isScreenWidthInRange(minWidth, maxWidth) {
+    const screenWidth = window.innerWidth;
+    return screenWidth >= minWidth && screenWidth <= maxWidth;
+  }
+  
+  // Function to initialize Swiper instance
+  function initializeSwiper() {
+    const swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+          modifierClass: "white-pagination"
+        },
+      });
+  }
+  
+  // Check screen width and initialize Swiper if it's within the desired range
+  function checkScreenWidthAndInitializeSwiper() {
+    if (isScreenWidthInRange(375, 767)) {
+      initializeSwiper();
+    }
+  }
+  
+  // Call the function when the window loads and when it resizes
+  window.onload = checkScreenWidthAndInitializeSwiper;
+  window.onresize = checkScreenWidthAndInitializeSwiper;
