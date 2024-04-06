bc6c5a0887c5b172804700fda9a41d4551286a70
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-06 16:25:40 +0300

    Add tope-sellers-swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,33 @@
-
 function isScreenWidthInRange(minWidth, maxWidth) {
-    const screenWidth = window.innerWidth;
-    return screenWidth >= minWidth && screenWidth <= maxWidth;
+  const screenWidth = window.innerWidth;
+  return screenWidth >= minWidth && screenWidth <= maxWidth;
+}
+
+// Function to initialize Swiper instance
+function initializeSwiper() {
+  const ingredientsSwiper = new Swiper('.ingredients-swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  });
+}
+
+// Check screen width and initialize Swiper if it's within the desired range
+function checkScreenWidthAndInitializeSwiper() {
+  if (isScreenWidthInRange(375, 767)) {
+    initializeSwiper();
+  } else if(window.innerWidth >= 768 && window.innerWidth < 1136) {
+    const topSellersSwiper = new Swiper('.ingredients-swiper', {
+      slidesPerView: 2,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
   }
-  
-  // Function to initialize Swiper instance
-  function initializeSwiper() {
-    const swiper = new Swiper(".mySwiper", {
-        pagination: {
-          el: ".swiper-pagination",
-          dynamicBullets: true,
-        },
-      });
-  }
-  
-  // Check screen width and initialize Swiper if it's within the desired range
-  function checkScreenWidthAndInitializeSwiper() {
-    if (isScreenWidthInRange(320, 767)) {
-      initializeSwiper();
-    }
-  }
-  
-  // Call the function when the window loads and when it resizes
-  window.onload = checkScreenWidthAndInitializeSwiper;
-  window.onresize = checkScreenWidthAndInitializeSwiper;
+}
+
+// Call the function when the window loads and when it resizes
+window.onload = checkScreenWidthAndInitializeSwiper;
+window.onresize = checkScreenWidthAndInitializeSwiper;
