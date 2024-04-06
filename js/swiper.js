45648742e7edcf734a659dd62f97fb86c7fb97e8
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-07 02:44:02 +0300

    experimenting with swipers

--- js/swiper.js
+++ js/swiper.js
@@ -50,19 +50,33 @@ let reviewsSwiper;
 
 function initializeSwipers() {
   if (window.innerWidth <= 767) {
-    ingredientsSwiper = new Swiper('.ingredients-swiper', {
-      pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
+    // ingredientsSwiper = new Swiper('.ingredients-swiper', {
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     dynamicBullets: true,
+    //   },
+    // });
+
+    ingredientsSwiper =  new Swiper(".ingredients-swiper", {
+      grabCursor: true,
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: [0, 0, -400],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
       },
     });
 
-    topSellersSwiper = new Swiper('.top-sellers-swiper', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
+    // topSellersSwiper = new Swiper('.top-sellers-swiper', {
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   },
+    // });
 
     reviewsSwiper = new Swiper('.reviews-swiper', {
       pagination: {
@@ -70,16 +84,38 @@ function initializeSwipers() {
         clickable: true,
       },
     });
+
+    topSellersSwiper = new Swiper(".top-sellers-swiper", {
+      effect: "cards",
+      grabCursor: true,
+    })
   }
 
   if (window.innerWidth >= 768 && window.innerWidth <= 1135) {
-    topSellersSwiper = new Swiper('.top-sellers-swiper', {
-      slidesPerView: 2,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+    // topSellersSwiper = new Swiper('.top-sellers-swiper', {
+    //   slidesPerView: 2,
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   },
+    // });
+
+    topSellersSwiper = new Swiper(".top-sellers-swiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
       },
-    });
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    })
 
     reviewsSwiper = new Swiper('.reviews-swiper', {
       slidesPerView: 2,
@@ -89,6 +125,8 @@ function initializeSwipers() {
       },
     });
   }
+
+
 }
 
 // Initial execution
