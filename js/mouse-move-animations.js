e0e0fcb2bce50eb766c98873aad1dca1dc552938
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-20 15:20:50 +0300

    remove redundant files

--- js/mouse-move-animations.js
+++
@@ -1,8 +0,0 @@
-// const cursor = document.querySelector('.cursor');
-// const cursor2 = document.querySelector('.cursor2');
-
-// document.addEventListener('mousemove', customCursor);
-
-// function customCursor(event) {
-// cursor.style.cssText = cursor2.style.cssText = `left:${event.clientX}px; top:${event.clientY}px`;
-// }
